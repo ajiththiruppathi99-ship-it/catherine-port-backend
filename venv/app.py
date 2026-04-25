604c3c71d6cc6c9d6f0e405ab20959a4dbabc383
ajiththiruppathi99-ship-it <ajiththiruppathi99@gmail.com>
2026-04-25 17:54:05 +0530

    added home route + render fix

--- venv/app.py
+++ venv/app.py
@@ -7,6 +7,10 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Backend is running!"
+
 FILE_PATH = "data/contacts.xlsx"
 
 @app.route('/contact', methods=['POST'])
@@ -42,4 +46,5 @@ def save_contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
